8b4cec14d27cf25240f654a4eb4fdee8338552cb
Davi <hoanghienle69@gmail.com>
2024-06-08 16:28:23 +0700

    chore: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        peach: {
+          0 : "#F2E8DA",
+          1 : "#FFBE98",
+          2 : "#DCBD9E",
+          3 : "#EFCFBA",
+          4 : "#FA9A85",
+          5 : "#F97272",
+          6 : "#DE8286",
+          7 : "#FFB2A5",
+        }
+      }
+    },
   },
   plugins: [],
 }
