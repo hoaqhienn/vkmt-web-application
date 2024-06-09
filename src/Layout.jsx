ba11dcdfd2545ef5f2c4d1acd774d5a4f4356405
Davi <hoanghienle69@gmail.com>
2024-06-09 22:48:28 +0700

    Update header UI

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,8 @@
 // src/Layout.jsx
 import { Outlet, useNavigate } from "react-router-dom";
 import Logo from "./assets/logo.svg";
+import Avatar from "./assets/avatar.png";
+
 import ThemeToggle from "./components/ThemeToggle";
 import { useState } from "react";
 const Layout = () => {
@@ -16,16 +18,33 @@ const Layout = () => {
       }`}
     >
       <ThemeToggle setDarkMode={setDarkMode} darkMode={darkMode} />
-      <header className="flex items-center justify-center">
-        <button className="py-20" onClick={handleClick}>
+      <header className="flex w-full p-5 items-center justify-between border-b border-red-500">
+        <button className="" onClick={handleClick}>
           <img src={Logo} alt="VKMT Logo" width="80" height="45" />
+        </button>
+        <div>
+          <button className="mx-2" onClick={handleClick}>
+            Home
+          </button>
+          <button className="mx-2" onClick={handleClick}>
+            About
+          </button>
+          <button className="mx-2" onClick={handleClick}>
+            Services
+          </button>
+          <button className="mx-2" onClick={handleClick}>
+            Contact
+          </button>
+        </div>
+        <button className="" onClick={handleClick}>
+          <img src={Avatar} alt="Avatar" width="50" height="auto" />
         </button>
       </header>
       <main className="flex items-center justify-center">
         <Outlet />
       </main>
       <footer className="flex py-20 items-center justify-center">
-        <p>Footer</p>
+        <p className="text-xs">Copyright © 2024 VKMT</p>
       </footer>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         peach: {
-          0 : "#F2E8DA",
-          1 : "#FFBE98",
-          2 : "#DCBD9E",
-          3 : "#EFCFBA",
-          4 : "#FA9A85",
-          5 : "#F97272",
-          6 : "#DE8286",
-          7 : "#FFB2A5",
-        }
-      }
+          0: "#F2E8DA",
+          1: "#FFBE98",
+          2: "#DCBD9E",
+          3: "#EFCFBA",
+          4: "#FA9A85",
+          5: "#F97272",
+          6: "#DE8286",
+          7: "#FFB2A5",
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
